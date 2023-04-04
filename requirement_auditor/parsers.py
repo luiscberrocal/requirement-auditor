8b2a5e5dc06ca39c7bf2b14007e47b71dd8265e6
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-04-03 19:46:02 -0500

    Parser tests

--- requirement_auditor/parsers.py
+++ requirement_auditor/parsers.py
@@ -9,7 +9,7 @@ from .settings import FULLY_PINNED_REGEX
 def parse_line(line: str, line_number: int, regexp: Pattern = FULLY_PINNED_REGEX) -> ParsedLine:
     var_names = ['name', 'version', 'comment']
     match = regexp.match(line)
-    requirement_line_dict = {'raw': line, 'line_number': line_number, 'pinned': None}
+    requirement_line_dict = {'raw': line.replace('\n', ''), 'line_number': line_number, 'pinned': None}
     if match:
         requirement_line_dict['pinned'] = dict()
         for var_name in var_names:
@@ -18,11 +18,11 @@ def parse_line(line: str, line_number: int, regexp: Pattern = FULLY_PINNED_REGEX
     return requirement_line
 
 
-
-def parse_requirement_file(req_file: Path, regexp: Pattern = FULLY_PINNED_REGEX) -> List[Dict[str, Any]]:
+def parse_requirement_file(req_file: Path, regexp: Pattern = FULLY_PINNED_REGEX) -> List[ParsedLine]:
     with open(req_file, 'r') as r_file:
         lines = r_file.readlines()
     parsed_requirements = list()
     for i, line in enumerate(lines, 1):
-        parsed_requirements.append(requirement_line)
+        parsed_line = parse_line(line, i, regexp=regexp)
+        parsed_requirements.append(parsed_line)
     return parsed_requirements
