b57dcf734fee0969f3b5ab59b4c1220440ef5381
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-04-09 10:13:53 -0500

    Database update not working.

--- requirement_auditor/__init__.py
+++ requirement_auditor/__init__.py
@@ -4,6 +4,13 @@ __author__ = """Luis C. Berrocal"""
 __email__ = 'luis.berrocal.1942@gmail.com'
 __version__ = '2.0.0'
 
+from pathlib import Path
+
 from requirement_auditor.config.configuration import ConfigurationManager
+from requirement_auditor.db.databases import JSONRequirementDatabase
 
 CONFIGURATION_MANAGER = ConfigurationManager()
+db_file = Path(CONFIGURATION_MANAGER.get_current()['application']['database_file']['file'])
+DATABASE = JSONRequirementDatabase(db_file)
+
+del db_file
--- requirement_auditor/db/cli_commands.py
+++ requirement_auditor/db/cli_commands.py
@@ -10,7 +10,12 @@ Add one requirement
 requirement-auditor database add --name django
 
 """
+from pprint import pprint
+
 import click
+
+from requirement_auditor import CONFIGURATION_MANAGER, DATABASE
+from requirement_auditor.db.managers import update_single_requirement, update_requirements
 
 
 @click.group()
@@ -22,6 +27,16 @@ def database():
 @click.option('-n', '--name')
 def update(name: str | None = None) -> None:
     print('Update database')
+    if name is None:
+        requirement = DATABASE.get(name)
+        if requirement is None:
+            click.secho(f'No requirement {name} found.', fg='yellow')
+        req, updated = update_single_requirement(requirement)
+    else:
+        requirements_to_update = update_requirements(DATABASE)
+        for req in requirements_to_update:
+            msg = f'{req.name}'
+            click.secho(msg, fg='green')
 
 
 database.add_command(update)
