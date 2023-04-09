0badd913b5e9275299bc58047ed10a5398cbccc5
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-04-09 10:31:48 -0500

    Test for udpating version.

--- tests/db/test_managers.py
+++ tests/db/test_managers.py
@@ -17,13 +17,15 @@ def test_update_single_requirement(mocker, json_db):
 
 
 def test_update_requirements(mocker, json_db):
-    def mock_get_latest_version(name: str):
+    def mock_get_latest_version(name: str, stable_only: bool):
         if name == 'django':
             return '5.2.0'
         else:
-            return '1.2.0'
+            req = json_db.get(name)
+            return req.latest_version
 
-    with mock.patch.object(mock_get_latest_version, 'requirement_auditor.db.managers.get_latest_version'):
-        updatable_requirements = update_requirements(json_db)
-        for req in updatable_requirements:
-            print(req.name)
+    mocked_fun = mocker.patch('requirement_auditor.db.managers.get_latest_version', side_effect=mock_get_latest_version)
+    updatable_requirements = update_requirements(json_db)
+    assert mocked_fun.call_count == 10
+    assert len(updatable_requirements) == 1
+    assert updatable_requirements[0].name == 'django'
