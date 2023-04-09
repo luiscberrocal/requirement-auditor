5221a2987e4fffe533de30303696943a5ca188f5
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-04-09 08:48:08 -0500

    Bump version: 1.0.2 → 2.0.0

--- requirement_auditor/__init__.py
+++ requirement_auditor/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Luis C. Berrocal"""
 __email__ = 'luis.berrocal.1942@gmail.com'
-__version__ = '1.0.2'
+__version__ = '2.0.0'
 
 from requirement_auditor.config.configuration import ConfigurationManager
 
