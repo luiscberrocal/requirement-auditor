5945c92a9047a9150cd8c2fff0e6eb30554ffea8
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-04-18 21:52:20 -0500

    Version comparison.

--- requirement_auditor/models.py
+++ requirement_auditor/models.py
@@ -3,6 +3,8 @@ from typing import Optional
 
 from pydantic import BaseModel, Field, HttpUrl
 
+from requirement_auditor.utils import convert_version_to_tuples
+
 
 def convert_datetime_to_iso_8601_with_z_suffix(dt: datetime) -> str:
     return dt.strftime('%Y-%m-%dT%H:%M:%SZ')
@@ -62,3 +64,35 @@ class ParsedLine(BaseModel):
     raw: str
     pinned: PinnedRequirement = Field(default=None)
     db_requirement: Optional[PythonRequirement] = Field(default=None)
+
+
+class VersionNumber(BaseModel):
+    major: int
+    minor: int
+    patch: Optional[int | str]
+
+    def __str__(self):
+        if self.patch is None:
+            return f'{self.major}.{self.minor}'
+        else:
+            return f'{self.major}.{self.minor}.{self.patch}'
+
+    def __ge__(self, other):
+        if type(other.patch) == type(self.patch):
+            return convert_version_to_tuples(str(self)) >= convert_version_to_tuples(str(other))
+        else:
+            condition1 = (self.major, self.minor) >= (other.major, other.minor)
+            if condition1 and isinstance(other.patch, str):
+                return True
+            else:
+                return False
+
+    def __gt__(self, other):
+        if type(other.patch) == type(self.patch):
+            return convert_version_to_tuples(str(self)) > convert_version_to_tuples(str(other))
+        else:
+            condition1 = (self.major, self.minor) > (other.major, other.minor)
+            if condition1 and isinstance(other.patch, str):
+                return True
+            else:
+                return False
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from requirement_auditor.models import PythonRequirement
+from requirement_auditor.models import PythonRequirement, VersionNumber
 
 
 def test_is_behind():
@@ -8,3 +8,25 @@ def test_is_behind():
                                     approved_version='2.0.0', last_updated=datetime.now())
     is_behind = requirement.approved_version_info < requirement.latest_version_info
     assert is_behind
+
+
+class TestVersionNumber:
+
+    def test_greater_than(self):
+        req1 = VersionNumber(major=2, minor=1, patch=1)
+        req2 = VersionNumber(major=2, minor=1, patch=12)
+
+        assert req2 > req1
+
+    def test_greater_than2(self):
+        req1 = VersionNumber(major=2, minor=1, patch=1)
+        req2 = VersionNumber(major=3, minor=1, patch='b-0')
+
+        assert req2 > req1
+
+
+    def test_greater_than3(self):
+        req1 = VersionNumber(major=3, minor=1, patch=1)
+        req2 = VersionNumber(major=3, minor=1, patch='b-0')
+
+        assert req1 > req2
